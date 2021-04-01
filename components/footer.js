a70bbb6a177100fa0114a22326c70bb63031dadc
kitajchuk <bk@kitajchuk.com>
2021-04-01 10:25:31 -0700

    Footer

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,40 @@
+import Link from 'next/link';
+import { Button } from './jambonz-ui';
+import { homeObj } from '../lib/vars';
+
+function FooterItem({ obj }) {
+  return (
+    <li>
+      <Link href={obj.link}>
+        <a target={obj.open ? '_blank' : null} className="foot__link">
+          {obj.label}
+        </a>
+      </Link>
+    </li>
+  );
+}
+
 export default function Footer({ siteData }) {
   return (
-    <footer className="foot"></footer>
+    <footer className="bg-jambonz foot">
+      <div className="wrap foot__wrap">
+        <div className="foot__navs">
+          <ul className="foot__links">
+            {siteData.footer.links.map((obj) => {
+              return <FooterItem key={obj.id} obj={obj} />
+            })}
+          </ul>
+          <ul className="foot__links">
+            <FooterItem key="home" obj={homeObj} />
+            {siteData.navi.links.map((obj) => {
+              return <FooterItem key={obj.id} obj={obj} />
+            })}
+          </ul>
+        </div>
+        <div className="foot__support">
+          <Button href={`mailto:${siteData.footer.email}`} target="_blank" subStyle="light">{siteData.footer.email}</Button>
+        </div>
+      </div>
+    </footer>
   );
 }
