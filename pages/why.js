09e09485cea23b52b07b2255488f27102bfdd4bc
kitajchuk <bk@kitajchuk.com>
2021-04-05 16:17:52 -0700

    Why jambonz static page

--- pages/why.js
+++ pages/why.js
@@ -1,5 +1,5 @@
 import Layout from '../components/layout';
-import { Hero, Icon, H5, P } from '../components/jambonz-ui';
+import { Hero, Icon, Button, H5, H2, P } from '../components/jambonz-ui';
 import { getData } from '../lib/data';
 import { nanoid } from 'nanoid';
 
@@ -23,11 +23,44 @@ function Facts({data}) {
   );
 }
 
+function OS({data}) {
+  return (
+    <div className="os pad">
+      <div className="wrap os__wrap">
+        <div className="os__headline">
+          <H2>{data.headline}</H2>
+        </div>
+        <div className="os__subtext">
+          <H5>{data.subtext}</H5>
+        </div>
+        <div className="btns os__btns">
+          {data.buttons.map((button) => {
+            return (
+              <Button key={nanoid()} href={button.url} target="_blank" style="pill">
+                <Icon name={button.icon} />
+                <span>{button.text}</span>
+              </Button>
+            );
+          })}
+        </div>
+        <div className="os__logo">
+          <img src={data.logo} />
+          <Icon name={data.icon} style="pill" />
+        </div>
+        <div className="os__cta">
+          <Button href={data.url} subStyle="dark" target="_blank">{data.cta}</Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export default function Why({ data }) {
   return (
     <Layout siteData={data.site}>
       <Hero data={data.why.hero} subStyle="why" />
       <Facts data={data.why.facts} />
+      <OS data={data.why.os} />
     </Layout>
   );
 }
