a24d6560343dd81e7b87a6b1c9206784e493640b
kitajchuk <bk@kitajchuk.com>
2021-04-05 17:16:06 -0700

    Pricing page touts

--- pages/pricing.js
+++ pages/pricing.js
@@ -1,11 +1,30 @@
 import Layout from '../components/layout';
-import { Hero } from '../components/jambonz-ui';
+import { Hero, Icon, P } from '../components/jambonz-ui';
 import { getData } from '../lib/data';
+import { nanoid } from 'nanoid';
+
+function Touts({data}) {
+  return (
+    <div className="bg-pink touts">
+      <div className="wrap touts__wrap">
+        {data.map((tout) => {
+          return (
+            <div key={nanoid()} className="touts__item">
+              <Icon name={tout.icon} style="fill" subStyle={tout.color} />
+              <P>{tout.text}</P>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
 
 export default function Pricing({ data }) {
   return (
     <Layout siteData={data.site}>
       <Hero data={data.pricing.hero} subStyle="pricing" />
+      <Touts data={data.pricing.touts} />
     </Layout>
   );
 }
