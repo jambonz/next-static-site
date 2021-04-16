3e01377471beefe2e478d20ff8ffa86a7e49b74e
kitajchuk <bk@kitajchuk.com>
2021-04-15 20:13:06 -0700

    Fix active navi link -- regex FTW

--- components/navi.js
+++ components/navi.js
@@ -9,9 +9,10 @@ import { Button, Icon, useMobileMedia } from './jambonz-ui';
 
 function NaviItem({obj}) {
   const router = useRouter();
+  const regex = new RegExp( `^${obj.link}` );
   const classes = {
     navi__link: true,
-    active: (router.asPath === obj.link),
+    active: regex.test( router.asPath ),
   };
 
   return (
