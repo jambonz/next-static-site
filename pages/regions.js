4d2a4900a30b754d6e612a4ee754ac383f88db8c
khang <khanghugo123@gmail.com>
2022-06-13 13:36:30 -0400

    changes??

--- pages/regions.js
+++ pages/regions.js
@@ -3,7 +3,7 @@ import { useRouter } from 'next/router';
 import { nanoid } from 'nanoid';
 
 import Layout from '../src/components/layout';
-import { Hero, Icon, P } from '../src/components/jambonz-ui';
+import { Hero, Icon, P, H6 } from '../src/components/jambonz-ui';
 import { getData } from '../src/lib/data';
 
 function Regions({ data }) {
@@ -20,18 +20,24 @@ function Regions({ data }) {
             <div className="regions__icon">
               <Icon name={region.icon} subStyle={region.color} mainStyle="fill" />
             </div>
-            <P className="h6">
-              <span className={`color--${region.color}`}>{region.title}</span>
-            </P>
-            <div className="i">
-              <Link href={href}>
-                <a title={`jambonz ${region.title}`}>
-                  <P className="h6">
-                    <span className="regions__text">{region.text}</span>
-                    <Icon name="ExternalLink" />
-                  </P>
-                </a>
-              </Link>
+            <div className="regions__title">
+              <P className="h6">
+                <span className={`color--${region.color}`}>
+                  {region.title}
+                </span>
+              </P>
+            </div>
+            <div className="regions__text">
+              <P className="h6">
+                <Link href={href}>
+                  <a title={`jambonz ${region.title}`}>
+                    <P className="i">
+                      <span className="regions__text">{region.text}</span>
+                      <Icon name="ExternalLink" />
+                    </P>
+                  </a>
+                </Link>
+              </P>
             </div>
           </div>
         );
