cc675d53ed9877fb0b74935e33a65c219f482306
Brandon Lee Kitajchuk <bk@kitajchuk.com>
2022-06-28 17:53:10 -0700

    Update utils.ts

--- jambonz-ui/src/js/utils.ts
+++ jambonz-ui/src/js/utils.ts
@@ -1,8 +1,8 @@
-export interface ClassNameObjectType {
+export interface ClassNameObject {
   [key: string]: boolean;
 }
 
-export const classNames = (o: ClassNameObjectType) => {
+export const classNames = (o: ClassNameObject) => {
   return Object.keys(o)
     .filter(k => (o[k] === true))
     .join(' ');
