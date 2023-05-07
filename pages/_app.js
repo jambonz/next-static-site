574c9dd427d2b9261e565936ca98bb2e8294e93e
Dave Horton <daveh@beachdognet.com>
2023-05-07 09:11:35 -0400

    add support for google tag manager

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import '../src/styles/global.scss';
+import { useEffect } from 'react'
+import TagManager from 'react-gtm-module'
+
+const gtmId = 'G-7J55WH6BVM';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    TagManager.initialize({ gtmId })
+  }, []);
+  return <Component {...pageProps} />
 }
